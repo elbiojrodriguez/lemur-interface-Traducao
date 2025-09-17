10e8563af57bb96921c70511cc2565295d347dcd
elbiojrodriguez <51291186+elbiojrodriguez@users.noreply.github.com>
2025-09-17 20:28:02 -0300

    Update media-manager.js

--- js/media-manager.js
+++ js/media-manager.js
@@ -1,8 +1,12 @@
-// js/media-manager.js
 let mediaStream = null;
 
 export async function getMediaStream() {
   if (mediaStream) return mediaStream;
+
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    console.error("getUserMedia não é suportado neste navegador.");
+    throw new Error("getUserMedia não suportado");
+  }
 
   try {
     mediaStream = await navigator.mediaDevices.getUserMedia({
