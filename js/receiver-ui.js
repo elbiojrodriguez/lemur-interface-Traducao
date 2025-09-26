5340dc287dd1241cbe02e5c71f300582905f9b70
elbiojrodriguez <51291186+elbiojrodriguez@users.noreply.github.com>
2025-09-26 17:43:40 -0300

    Update receiver-ui.js

--- js/receiver-ui.js
+++ js/receiver-ui.js
@@ -96,8 +96,8 @@ window.onload = async () => {
                 elemento.style.transition = 'opacity 0.5s ease'; // ← Transição suave
                 
                 // ✅ PULSAÇÃO IDÊNTICA AO CALLER:
-                elemento.style.animation = 'pulsar-flutuar 2s infinite';
-                elemento.style.backgroundColor = 'rgba(76, 175, 80, 0.2)'; // Verde bem fraquinho
+                elemento.style.animation = 'pulsar-crescer 1.5s infinite';
+                elemento.style.backgroundColor = 'rgba(76, 175, 80, 1)'; // Verde bem fraquinho
             }
 
             if (window.speechSynthesis) {
